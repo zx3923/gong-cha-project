12b821a3050694128dc876690ec26b8c84eb4cc6
kei781 <shtkddns5395@gmail.com>
2022-08-24 01:56:55 +0900

    중요 컴포넌트제외 레이아웃 잡음

--- src/Compornent/Contents/Contents.jsx
+++ src/Compornent/Contents/Contents.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import style from "./Contents.module.css";
+import Brand from "./pages/Brand/Brand";
 const Contents = () => {
-  return <div className={style.Contents}></div>;
+  return (
+    <div className={style.Contents}>
+      <Brand />
+    </div>
+  );
 };
 
 export default Contents;
